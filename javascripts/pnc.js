f18a34242f16c48966f63a341ac57c713dc45df3
danieledwardkim <daniel.edward.kim.email@gmail.com>
2016-01-11 19:04:24 -0500

    added2

---
+++ javascripts/pnc.js
@@ -0,0 +1,85 @@
+(function(angular) {
+
+	var app = angular.module('app', [ 'ngRoute', 'ngAnimate', 'ngTouch' ]);
+	
+	var appUIState = {
+		expanded : false
+	};
+	
+	app.factory("uiState",function($timeout){
+        return appUIState;
+	});
+	
+	
+	app.config(function($routeProvider, $locationProvider) {
+		$routeProvider.when('/cmt/:section', {
+			templateUrl : function(params) {
+				appUIState.expanded = false;
+				return "/partials/" + params['section'] + ".html";
+			},
+			reloadOnSearch : false
+		})
+		.when('/onboarding/:section',{
+			templateUrl : function(params) {
+				appUIState.expanded = false;
+				return "/partials/onboarding" + params['section'] + ".html";
+			},
+			reloadOnSearch : false
+		})
+		.otherwise({
+			redirectTo : function(params, path, search) {
+				return "/cmt/dashboard";
+			}
+		});
+		$locationProvider.html5Mode({
+			enabled : true,
+			requireBase : false
+		});
+	});
+
+	app.controller('UIController', function($scope, $timeout, $location, uiState) {
+		var module = this;
+		$scope.uiState = uiState;
+		function currentSection() {
+			var currentSection = $location.path().substring(5); // /cmt/<section>
+			var menuItems = $('.menuItem a');
+			var current;
+			var currentIndex;
+			for(var i=0; i<menuItems.length; i++) {
+				if($(menuItems[i]).attr('href') == currentSection) {
+					console.log("Index = "+i);
+					currentIndex = i;
+					current = $(menuItems[i]);
+					break;
+				}
+			}
+			var prevIndex = currentIndex-1;
+			if(prevIndex < 0) {
+				prevIndex = menuItems.length - 1;
+			}
+			var nextIndex = currentIndex+1;
+			if(nextIndex >=  menuItems.length) {
+				nextIndex = 0;
+			}
+			return {
+				prevIndex: prevIndex,
+				nextIndex: nextIndex,
+				prevURI: '/cmt/'+$(menuItems[prevIndex]).attr('href'),
+				nextURI: '/cmt/'+$(menuItems[nextIndex]).attr('href')
+				
+			}
+		}
+		
+		$scope.previousSection = function() {
+			$location.path(currentSection().prevURI);
+		}
+		
+		$scope.nextSection = function() {
+			$location.path(currentSection().nextURI);
+		}
+		
+	});
+
+	angular.bootstrap(document, [ 'app' ]);
+
+})(angular);
